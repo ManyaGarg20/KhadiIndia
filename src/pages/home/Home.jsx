1d6efbc2d7621aa3ec6d275206fa3dce66533769
ManyaGarg20 <manyagarg2002@gmail.com>
2023-11-17 00:09:50 +0530

    productcard

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,6 +4,7 @@ import Layout from '../../components/layout/Layout'
 import myContext from '../../context/data/myContext';
 import HeroSection from '../../components/HeroSection/HeroSection';
 import Filter from '../../components/filter/Filter'
+import ProductCard from '../../components/productCard/ProductCard';
 
 function Home() {
 const context = useContext(myContext);
@@ -13,6 +14,7 @@ const {state ,color} = context;
     <Layout >
            <HeroSection />
            <Filter />
+           <ProductCard/> 
             </Layout>
   )
 }
