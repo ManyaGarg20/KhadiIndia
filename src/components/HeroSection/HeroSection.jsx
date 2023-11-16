38f28096cb326c9de5cd415f4986189c6de9f76b
ManyaGarg20 <manyagarg2002@gmail.com>
2023-11-16 23:19:51 +0530

    img

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function HeroSection() {
   return (
     <div>
-        <img style={{width:"100vw" , height:"50vh"}} src="https://img.etimg.com/thumb/width-1600,height-900,imgsize-574369,resizemode-75,msid-98172870/industry/services/retail/over-55-of-small-town-residents-preferred-online-shopping-for-gifts-clothing-and-footwear-survey.jpg" alt="imgHeroSection" />
+        <img style={{width:"100vw" , height:"65vh"}} src="https://img.etimg.com/thumb/width-1600,height-900,imgsize-574369,resizemode-75,msid-98172870/industry/services/retail/over-55-of-small-town-residents-preferred-online-shopping-for-gifts-clothing-and-footwear-survey.jpg" alt="imgHeroSection" />
 
     </div>
   )
